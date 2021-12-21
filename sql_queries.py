0b04fa9d27e8efcfcbca40fd48b63a90bce2cc85
Lucas Madrid <madridlucas75@gmail.com>
2021-12-21 19:43:17 -0300

    modfiying notebook and sql queries to meet project Rubric Specification

--- sql_queries.py
+++ sql_queries.py
@@ -8,39 +8,38 @@ drop_visa_table = "DROP TABLE IF EXISTS DimVisa;"
 
 create_immigration_table = """
     CREATE TABLE IF NOT EXISTS FactImmigration(
-        imm_id					DOUBLE PRECISION PRIMARY KEY,
-        port					VARCHAR(250),
-        country_origin			DOUBLE PRECISION,
-        immigration_type		DOUBLE PRECISION,
+        imm_id					DOUBLE PRIMARY KEY,
+        port					VARCHAR(250) NOT NULL,
+        country_origin			VARCHAR(250),
+        arrival_date			INT NOT NULL,
+        departure_date			INT,
+        immigration_type		INT,
         temp_state_residence	VARCHAR(15),
-        gender					VARCHAR(10),
-        visa_type				DOUBLE PRECISION,
-        visa_category			VARCHAR(10),
-        arrival_date_sas			DOUBLE PRECISION,
-        departure_date_sas			DOUBLE PRECISION
+        gender					VARCHAR(10) CHECK( gender IN ('F','M','X','U') ),
+        visa_type				VARCHAR(15),
+        visa_category			VARCHAR(10)
 
     );
 """
 
 create_time_table = """
     CREATE TABLE IF NOT EXISTS DimTime (
-        sas_date_id			DOUBLE PRECISION PRIMARY KEY,
-        arrival_date_f		VARCHAR(30),
+        sas_date_id			DOUBLE PRIMARY KEY,
         day					INT,
         week				INT,
         month				INT,
         year				INT,
-        weekday             INT,
-        quarter				INT
+        quarter				INT CHECK ( quarter >=1 and quarter <=4 ),
+        arrival_date_f		DATE
     );
 """
 
 create_airport_table = """
     CREATE TABLE IF NOT EXISTS DimAirport (
         local_code		VARCHAR(100) PRIMARY KEY,
-        airport_name	VARCHAR(250),
-        airport_type 	VARCHAR(100),
-        iso_country		VARCHAR(10),
+        airport_name	VARCHAR(250) NOT NULL,
+        airport_type 	VARCHAR(100) NOT NULL,
+        iso_country		VARCHAR(10) NOT NULL,
         continent		VARCHAR(30),
         municipality	varchar(50)
     );
@@ -48,22 +47,22 @@ create_airport_table = """
 
 create_country_table = """
     CREATE TABLE IF NOT EXISTS DimCountry (
-        country_code	DOUBLE PRECISION PRIMARY KEY,
-        country_name	VARCHAR(150)
+        country_code	VARCHAR(50) PRIMARY KEY,
+        country_name	VARCHAR(150) NOT NULL
     );
 """
 
 create_state_table = """
     CREATE TABLE IF NOT EXISTS DimState (
-        state_code			VARCHAR(20) PRIMARY KEY,
-        state_description	VARCHAR(100),
-        country_code		VARCHAR(20)
+        state_code			VARCHAR(10) PRIMARY KEY,
+        state_description	VARCHAR(100) NOT NULL,
+        country_code		VARCHAR(10)
     );
 """
 
 create_visa_table = """
     CREATE TABLE IF NOT EXISTS DimVisa (
-        visa_category				VARCHAR(250) PRIMARY KEY,
+        visa_category				VARCHAR(50) PRIMARY KEY,
         description					TEXT,
         initial_duration_of_staya	TEXT,
         annual_numeric_limit		VARCHAR(250)
